1007a3d8dff30cf16caf9dcf96666468f2f16b84
kubrakhmd <kubraxe-bp217@code.edu.az>
2024-10-04 14:59:44 +0400

    homework04.10

---
+++ task04.10/task04.10/Program.cs
@@ -0,0 +1,35 @@
+﻿#region minimumun tapilmasi
+//int[] arr = { 15, 23, 3, 45 };
+//int min = arr[0];
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] < min)
+//    {
+//        min = arr[i];
+//    }
+
+//}
+//Console.WriteLine($"ededin minimumudur {min}");
+
+#endregion
+#region massivin elementleri cemi
+//int[]arr = { 35, 42, 24 };
+//int cem1 = 0;
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    cem1 += arr[i];
+
+//}
+//Console.WriteLine(cem1);
+
+#endregion
+
+//string[] names = { "leyla", "nigar", "fatime" , "eli" };
+//for (int i = 0; i < names.Length; i++)
+//{
+//  if (names[i].Length > 4)
+//    {
+//        Console.WriteLine(names[i]);
+//    }
+//}
+
