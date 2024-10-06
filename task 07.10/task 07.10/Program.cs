638702e268c770ab6a28facf2bfd5937324019a0
kubrakhmd <kubraxe-bp217@code.edu.az>
2024-10-06 19:37:08 +0400

    task 07.10

---
+++ task 07.10/task 07.10/Program.cs
@@ -0,0 +1,96 @@
+﻿#region murekkeb ededler
+//int[] arr = { 3, 5, 12, 18 };
+//for (int i = 0; i < arr.Length; i++)
+//{
+//   for (int j = 2; j < arr[i];j++)
+//{
+//   if (arr[i] % j == 0)
+//        {
+// Console.WriteLine(arr[i]);
+// break;
+//        }   
+//}
+//} 
+#endregion
+#region beraber olan elementlerin tapilmasi
+//int[] nums = { 5, 10, 15, 30 };
+//int[] arr = { 5, 6, 10, 12 };
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    for (int j = 0; j < arr.Length; j++)
+//    {
+//    if (nums[i] == arr[j])
+//        {
+//Console.WriteLine(nums[i]);
+//        }
+//    }
+//} 
+#endregion
+//using System.Security.Cryptography;
+
+//int[] arr = { 7, 9, 10, 15, 20 };
+//int max = arr [0];
+//int min = arr [0];
+//for (int i = 0; i < arr.Length; i++)
+//{
+//    if (arr[i] > max)
+//    {
+//        max = arr[i];
+//        arr[i] = max;
+
+//    }
+//    for (int j = 0; j < arr.Length; j++)
+//    {
+//        if (arr[j] < min)
+//        {
+//            min = arr[j];
+
+//        }
+//    }
+//}
+//Console.WriteLine(max);
+//Console.WriteLine(min);
+#region ededin reqenmleri cemi
+//byte num= 167;
+//int vuruq= 1;
+//int cem = 0;
+//bool exit= false;
+//while (!exit)
+//{
+//    int mod = num % (vuruq * 10);
+//    int qaliq = mod /vuruq;
+//    cem+= qaliq;
+//    vuruq *= 10;
+//    exit = (mod == num);
+//}
+//Console.WriteLine(cem);
+
+#endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
